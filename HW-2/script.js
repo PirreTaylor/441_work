6eaea1c794fec9a25b8643dac88b4a362aa9f40e
Taylor Pirre <Missperidot1@gmail.com>
2019-01-26 20:51:19 -0700

    Finished HW2
    Finished main work of HW2. Now just to finish the readme.

--- HW-2/script.js
+++ HW-2/script.js
@@ -4,4 +4,5 @@ I hope I was able to get everything to work
 alright.
 -Taylor Pirre
 */
-console.log()
+
+console.log("Three of my favorite restaurants are Murry's, the No-Sweat Cafe, and Chef Mai's. Helena is pretty limited on its food choices, though. Three of my favorite snack foods are Tim-Tams, Chessmen Cookies, and hummus. At least one of those snacks is healthy. Can tea count as a snack? Three of my favorite fruits/veggies are mangoes, cucumbers, and spinach. Mangoes are good for the digestive system.");
