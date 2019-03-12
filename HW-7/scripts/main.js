630b990d7eb890b08dae7633568551cf1dba841d
Taylor Pirre <Missperidot1@gmail.com>
2019-03-11 18:42:24 -0600

    Constructors and Images
    Updating and fixing constructors/images.

--- HW-7/scripts/main.js
+++ HW-7/scripts/main.js
@@ -3,30 +3,35 @@ var myViewFinderArray = new Array();
 
 class ViewFinder
 {
-  constructor(title)
+  constructor(title, imagePath)
   {
+    //  initialize constructors here with the this. command
       this.title = title;
-      this.image1 = /images/Sea.jpg;
-
+      this.imagePath= imagePath;
   }
 
   toString()
   {
       return "Title: " + this.title;
-      return "image1: " + this.image1;
   }
-
+  // use getters to retrieve and activate constructors
   get theTitle()
   {
       return this.title;
+  }
+
+  get theimagePath()
+  {
+    return this.imagePath;
   }
 
 }
 
 function initializeArray()
 {
-    var myViewFinder = new ViewFinder("Surrealist Sealife");
-    var myViewFinder1 = new ViewFinder("The Age of Discovery");
+  //include everything the variable will be plugging in within the parentheses of the new command
+    var myViewFinder = new ViewFinder("Surrealist Sealife", "./images/Sea.jpg");
+    var myViewFinder1 = new ViewFinder("The Age of Discovery", "./images/Discovery.jpg");
     myViewFinderArray.push(myViewFinder);
     myViewFinderArray.push(myViewFinder1);
 
@@ -36,6 +41,8 @@ function accessInformation()
     /// random object from the array
     //console.log(myViewFinder.toString());
     //console.log(myViewFinder.theTitle)
-    document.getElementById("title").innerHTML = myViewFinderArray[1].toString();
+    //document.getElementById("title").innerHTML = myViewFinderArray[1].toString();
+    document.getElementById("title").innerHTML = myArray[0].theTitle;
+    document.getElementById("image").src = myArray[0].theImagePath;
 
 }
