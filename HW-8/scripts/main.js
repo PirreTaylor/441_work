f52035f998d6b8cccb1d3b84731bf082d1bed91c
Taylor Pirre <Missperidot1@gmail.com>
2019-03-14 20:05:35 -0600

    Move boat
    Move boat update.

--- HW-8/scripts/main.js
+++ HW-8/scripts/main.js
@@ -1,25 +1,3 @@
-var oceanSelector = "#ocean";
-var allOcean = new Array();
-class wavesInfo{
-    constructor(selector, imagePath)
-    {
-        this.selector = selector;
-        this.imagePath = imagePath;
-    }
-
-    get theSelector(){
-        return this.selector;
-    }
-
-    get theImagePath(){
-        return this.imagePath;
-    }
-
-    toString()
-    {
-        return this.selector + ":" + this.imagePath;
-    }
-}
 
 function initializeArray()
 {
@@ -27,8 +5,8 @@ function initializeArray()
 
 function moveBoat() {
 $(document).ready(function () {
-            $("button").click(function () {
-                $("#boatBox").animate({left: '200px'});
+            $("moveBoat").click(function () {
+                $("#boatBox").animate({left: '500px'});
             });
         });
       }
