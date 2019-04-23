da167d956d4277ecb4fd4cd21f6153b1134d5db2
Taylor Pirre <Missperidot1@gmail.com>
2019-04-23 00:28:31 -0600

    HW-8 Complete
    Homework 8 completed upload.

--- HW-8/scripts/main.js
+++ HW-8/scripts/main.js
@@ -16,6 +16,21 @@ class oceanWaves{
   }
 }
 
+class shipWaves{
+  constructor(imagePath){
+    this.imagePath = imagePath;
+  }
+
+  toString()
+  {
+
+  }
+  get theimagePath()
+  {
+    return this.imagePath;
+  }
+}
+
 
 function loadWaves(){
   var waves = new oceanWaves("images/waves2.png");
@@ -23,12 +38,29 @@ function loadWaves(){
   document.getElementById("waves2").src = waves.theimagePath;
 }
 
+function loadShipWaves(){
+  var speedWaves = new shipWaves("images/bowWaves.png");
+  console.log(speedWaves.theimagePath);
+  document.getElementById("bowWaves").src = speedWaves.theimagePath;
+}
 
 //function moveBoat() {
 $(document).ready(function () {
         loadWaves();
             $("#moveBoat").click(function () {
-                $("#boatBox").animate({left: '-=1000'});
+              $(this).hide();
+                $("#title1").hide();
+                $("#title2").toggle();
+                $("#boatBox").animate({left: '-=1000'}, "slow");
+                $("#bowWaves").toggle().animate({left: '-=1000'}, "slow").fadeToggle( function(){
+                  $("#title2").hide();
+                  $("#title3").fadeIn();
+                });
+                $("#clouds1").fadeOut("slow", function(){
+                  $("#clouds2").fadeIn("slow").fadeOut("slow", function(){
+                    $('#clouds3').fadeIn("slow").fadeOut("slow")
+                  });
+                });
 
             });
         });
