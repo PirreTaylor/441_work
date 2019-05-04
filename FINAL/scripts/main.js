004b0cbfbadc8ffad85f2a8d800824c280b34057
Taylor Pirre <Missperidot1@gmail.com>
2019-05-03 22:54:17 -0600

    Image Update
    image update

--- FINAL/scripts/main.js
+++ FINAL/scripts/main.js
@@ -1,33 +1,42 @@
 var myAdventureArray = new Array();
 var slideCounter = 0;
 
+//var canvas = document.getElementById("storyText");
+//var ctx = canvas.getContext("2d");
+
+
 class adventureScreen
 {
-  constructor(description){
-    //this.imagePath = imagePath;
+  constructor(description, imagePath){
     this.description = description;
+    this.imagePath = imagePath;
   }
 
   toString(){
-    //this.imagePath;
     return this.description;
   }
+
 
   get theDescription(){
     return this.description;
   }
+
+  get theImagePath(){
+    return this.imagePath = imagePath;
+  }
 }
+
 
 function initializeArray()
 {
-  var myAdventure = new adventureScreen("'Be good and don't let the rain get into your cybernetics this time.' These were my sister's last words to me before she left on that fated venture. What she had gone to look for, she never said.");
-  var myAdventure1 = new adventureScreen("It has been days since she left the base on that jourey. By all means, she should have been back by now...");
+  var myAdventure = new adventureScreen("'Be good and don't let the rain get into your cybernetics this time.' These were my sister's last words to me before she left on that fated venture. What she had gone to look for, she never said.", "images/adv1.jpg");
+  var myAdventure1 = new adventureScreen("It has been days since she left the base on that jourey. By all means, she should have been back by now...", "images/adv2.jpg");
   var myAdventure2 = new adventureScreen("What's this!? A distress signal?");
-  var myAdventure3 = new adventureScreen("This can't be good. I'll take this tracking device with me to pin down where it came from. Let's hope for the best...");
-  var myAdventure4 = new adventureScreen("You come upon a cave. There appears to have once been a locked door over its entrance that has been broken. Only one way to figure out what's inside...");
-  var myAdventure5 = new adventureScreen("The path ahead is dark and impossible to navigate by sight alone. You pull aside your eyepatch and enable the secondary LED reticle in your right eye to see better. You can see better now, but you also notice the new danger of-");
-  var myAdventure6 = new adventureScreen("A TERRITORIAL MECHA-MOTH! Think fast!");
-  var myAdventure7 = new adventureScreen("You have successfully battled and defeated the Mecha-Moth. You choose to assimilate its antennae into your arm to aid in your navigation. The antennae appear to have a sonar function, allowing you to find your way through the dark easier.");
+  var myAdventure3 = new adventureScreen("This can't be good. I'll take this tracking device with me to pin down where it came from. Let's hope for the best...", "images/blank.jpg");
+  var myAdventure4 = new adventureScreen("You come upon a cave. There appears to have once been a locked door over its entrance that has been broken. Only one way to figure out what's inside...", "images/blank.jpg");
+  var myAdventure5 = new adventureScreen("The path ahead is dark and impossible to navigate by sight alone. You pull aside your eyepatch and enable the secondary LED reticle in your right eye to see better. You can see better now, but you also notice the new danger of-", "images/blank.jpg");
+  var myAdventure6 = new adventureScreen("A TERRITORIAL MECHA-MOTH! Think fast!", "images/blank.jpg");
+  var myAdventure7 = new adventureScreen("You have successfully battled and defeated the Mecha-Moth. You choose to assimilate its antennae into your arm to aid in your navigation. The antennae appear to have a sonar function, allowing you to find your way through the dark easier.", "images/blank.jpg");
   myAdventureArray.push(myAdventure);
   myAdventureArray.push(myAdventure1);
   myAdventureArray.push(myAdventure2);
@@ -41,6 +50,7 @@ function initializeArray()
 function accessInformation()
 {
   document.getElementById("storyText").innerHTML = myAdventureArray[slideCounter].toString();
+  document.getElementById("imageBox").src = myAdventureArray[slideCounter].theImagePath()
   slideCounter++;
 }
 
---
+++ HW-10/scripts/main.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = 200;
+
+drawCircle();
+
+
+function update(){
+  drawCircle();
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  ctx.clearRect(0, 0, 800, 800);
+
+  drawCircle();
+  ctx.beginPath();
+  ctx.moveto(100,400);
+  ctx.arc(x, 200, 30, 0, Math.PI * 2, false);
+  ctx.strokeStyle = "#663300";
+  ctx.stroke();
+
+  x += 1;
+}
